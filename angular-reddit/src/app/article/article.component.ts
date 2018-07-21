f7e74892eb4523f4ec0b1e52682771a82a881860
James Anderson <jsondev1.0@outlook.com>
2018-07-21 08:22:36 -0400

    updating components and styling

--- angular-reddit/src/app/article/article.component.ts
+++ angular-reddit/src/app/article/article.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostBinding } from '@angular/core';
 
 @Component({
   selector: 'app-article',
@@ -6,8 +6,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./article.component.css']
 })
 export class ArticleComponent implements OnInit {
+  @HostBinding('attr.class') cssClass = 'row';
+  votes: number;
+  title: string;
+  link: string;
+  constructor() {
+    this.title = 'Angular 2';
+    this.link = 'http://angular.io';
+    this.votes = 10
+  }
 
-  constructor() { }
+  voteUp() {
+    this.votes += 1;
+  }
+
+  voteDown() {
+    this.votes -= 1;
+  }
 
   ngOnInit() {
   }
